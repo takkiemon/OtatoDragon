c299632ea7fbe899b5c41ba3041f8cbc0aa7048a
Itamar <itamar@live.nl>
2020-01-31 21:34:48 +0100

    click manager I guess

---
+++ Assets/Scripts/ClickBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBehaviour : MonoBehaviour
+{
+    private Color startcolor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // Execute something
+        Destroy(this.gameObject);
+    }
+
+
+    //highlights the object
+    void OnMouseEnter()
+    {
+        startcolor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+    
+    //undoes the highlight to the original
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startcolor;
+    }
+}
