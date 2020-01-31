c676221dc0a38f5bae80428471c2247b541bb02c
Vuong Pham <h.vuongpham@gmail.com>
2020-01-31 22:03:03 +0100

    Add GameManagerBehaviour

---
+++ Assets/Scripts/GameManagerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerBehaviour : MonoBehaviour
+{
+    public Text pollutionText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pollutionText.text = "pollution: " + GetPollution();
+    }
+
+    int GetPollution()
+	{
+        int totalPolution = 0;
+        CellBehaviour[] cellArray = this.gameObject.GetComponentsInChildren<CellBehaviour>();
+        foreach (CellBehaviour cell in cellArray)
+        {
+            totalPolution += cell.GetPollution();
+        };
+
+        return totalPolution;
+    }
+
+    bool IsWinConditionMet(int pollution)
+    {
+        return pollution == 0;
+    }
+}
