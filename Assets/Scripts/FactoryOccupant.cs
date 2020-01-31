6bfcc764ea1d8993e3aebed04c4ea06d0521b9e7
ezelking-LAPTOP <timroberson@live.nl>
2020-01-31 21:32:00 +0100

    Create interface of cell occupants and implement both factories and trees that return their appropriate polution values.

---
+++ Assets/Scripts/FactoryOccupant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryOccupant : ICellOccupant
+{
+    //Return the amount of polution caused by the occupying object.
+    public int Polution()
+    {
+        return 10;
+    }
+}
---
+++ Assets/Scripts/TreeOccupant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeOccupant : ICellOccupant
+{
+    //Return the amount of polution caused by the occupying object.
+    public int Polution()
+    {
+        return -10;
+    }
+}
