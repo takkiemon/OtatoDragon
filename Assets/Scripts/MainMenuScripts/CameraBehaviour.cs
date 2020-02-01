78ac12c3ccd02b28e5ce1c8ee0ff0a482ae2305a
Itamar <itamar@live.nl>
2020-02-01 16:40:06 +0100

    plain simple main menu, placeholder for TITLE now in place, how-to and about are empty.

---
+++ Assets/Scripts/MainMenuScripts/CameraBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 10.0f * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/MainMenuScripts/ExitButtonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonBehaviour : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
