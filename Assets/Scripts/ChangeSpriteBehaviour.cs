f65546c19cea1fdd809454cea2861b56f3454e88
Itamar <itamar@live.nl>
2020-02-01 15:03:59 +0100

    tier changes - tree and factory - methods could be more neat

---
+++ Assets/Scripts/ChangeSpriteBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSpriteBehaviour : MonoBehaviour
+{
+    public SpriteRenderer render;
+
+    public Sprite[] treeTiers;
+    public Sprite[] factoryTiers;
+
+    private int tierNr;
+
+    //private bool isCoroutineExecuting = false;
+
+    public float startTime;
+    public float repeatTime;
+
+    public bool isTree;
+
+    private void Start()
+    {
+        render = gameObject.GetComponent<SpriteRenderer>();
+
+        //sets the stage to 0, the first
+        tierNr = 0;
+
+        //sets the first stages
+        if (isTree) render.sprite = treeTiers[tierNr];
+        //more functions?
+
+        if(!isTree) render.sprite = factoryTiers[tierNr];
+        //more functions?
+
+        //start tiers after x seconds
+        InvokeRepeating("NextTier", startTime, repeatTime);
+    }
+
+    public void NextTier()
+    {
+        //sets the stage to the next one
+        tierNr++;
+
+        //sets to the next tier
+        if (isTree) render.sprite = treeTiers[tierNr];
+        //more functions?
+
+        if (!isTree) render.sprite = factoryTiers[tierNr];
+
+        //cancels the next stages when it's on the last stage
+        if (tierNr == 2)
+        {
+            CancelInvoke();
+        }
+    }
+
+    /*
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        if (isCoroutineExecuting)
+            yield break;
+
+        isCoroutineExecuting = true;
+
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        NextStage();
+
+        isCoroutineExecuting = false;
+    }
+    */
+}
